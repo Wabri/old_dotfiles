72e7317a3088543eb4c970da102f5ac00eaa19ce
wabri <gabriele.puliti@gmail.com>
2017-05-08 18:57:08 +0200

    add i3blocks, create a screenlocks lock much better

---
+++ i3/lock.sh
@@ -0,0 +1,9 @@
+icon="$HOME/.config/i3/screen.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -i "$tmpbg"
