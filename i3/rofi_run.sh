d30b7cbb5d618e8151586cb6e4565175ce3f0f2a
Wabri <gabriele.puliti@gmail.com>
2017-07-01 18:22:55 +0200

    added a new rofi setup

---
+++ i3/rofi_run.sh
@@ -0,0 +1,1 @@
+rofi -modi "window,run" -show run -font "System San Francisco Display 10" -hide-scrollbar -lines 10 -bw 20 -width 30
---
+++ i3/rofi_workspace.sh
@@ -0,0 +1,1 @@
+rofi -modi window -show window -font "System San Francisco Display 10" -hide-scrollbar -bw 20 -width 30
