a621554c253e765b9a62db966f188db6538c01c4
Wabri <gabriele.puliti@gmail.com>
2018-09-26 22:06:52 +0200

    update one more time debinstall.sh

--- debinstall.sh
+++ debinstall.sh
@@ -2,7 +2,7 @@ cd $HOME
 
 sudo apt update
 
-sudo apt install i3-wm i3 i3blocks i3lock-fancy i3-wm feh gnome-screenshot rofi lxappearance arandr snap flashplugin-installer libxss1 libappindicator1 libindicator7 thunar redshift-gtk
+sudo apt install i3-wm i3 i3blocks i3lock-fancy i3-wm feh gnome-screenshot rofi lxappearance arandr snap flashplugin-installer libxss1 libappindicator1 libindicator7 thunar redshift-gtk franz
 
 sudo snap install skype
 
@@ -44,32 +44,32 @@ sudo dpkg -i light.deb
 
 rm -r light.deb
 
-wget -O franz.deb https://github.com/meetfranz/franz/releases/download/v5.0.0-beta.18/franz_5.0.0-beta.18_amd64.deb
-
-sudo dpkg -i franz.deb
-
-rm -r franz.deb
-
 wget -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 sudo dpkg -i google-chrome.deb
 
 rm -r google-chrome.deb
 
-sudo apt --fix-broken install
+sudo apt install --fix-broken
 
 cd $HOME
 
+sudo rm -r dotfiles/
+
 git clone https://github.com/Wabri/dotfiles.git
 
-cp dotfiles/i3/* $HOME/.config/i3/
+mkdir .config/i3
 
-cp -r dotfiles/.fonts/ $HOME/.fonts/
+mkdir .fonts
 
-cp dotfiles/desktopImage/desktop.jpg $HOME/.config/i3/desktop.jpg
+cp $HOME/dotfiles/i3/* $HOME/.config/i3/
 
-cp dotfiles/scripts/batteryPopUp/* $HOME/.config/i3/
+cp -r $HOME/dotfiles/.fonts/ $HOME/.fonts/
 
-sudo cp dotfiles/scripts/spotifyWithFirefox/spotify /usr/local/bin/spotify
+cp $HOME/dotfiles/desktopImage/desktop.jpg $HOME/.config/i3/desktop.jpg
 
-rm -r dotfiles/
+cp $HOME/dotfiles/scripts/batteryPopUp/* $HOME/.config/i3/
+
+sudo cp $HOME/dotfiles/scripts/spotifyWithFirefox/spotify /usr/local/bin/spotify
+
+sudo rm -r dotfiles/
