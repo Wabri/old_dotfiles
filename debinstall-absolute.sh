64b2d4e309aeb1d8e065c5ca42273ce1a6344e88
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-11-05 19:17:25 +0100

    update debinstall

--- debinstall-absolute.sh
+++ debinstall-absolute.sh
@@ -38,6 +38,7 @@ echo "-----------------------------------------------"
 # Atom installer
 echo "ATOM INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/atom_installer.sh
+sudo chmod +x atom_installer.sh
 ./atom_installer.sh
 sudo apt install python-pip
 sudo pip install pep8 autopep8 flake8 flake8-docstrings hacking
@@ -55,6 +56,7 @@ echo "-----------------------------------------------"
 # Discord
 echo "DISCORD INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/discord_installer.sh
+sudo chmod +x discord_installer.sh
 ./discord_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -64,6 +66,7 @@ echo "-----------------------------------------------"
 # Dropbox
 echo "DROPBOX INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/dropbox_installer.sh
+sudo chmod +x dropbox_installer.sh
 ./dropbox_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -73,6 +76,7 @@ echo "-----------------------------------------------"
 # EclipseInstaller
 echo "ECLIPSE INSTALLER INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/eclipseInstaller_installer.sh
+sudo chmod +x eclipseInstaller_installer.sh
 ./eclipseInstaller_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -82,6 +86,7 @@ echo "-----------------------------------------------"
 # GitKraken
 # echo "GITKRAKEN INSTALLER"
 # wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/gitkraken_installer.sh
+# sudo chmod +x gitkraken_installer.sh
 # ./gitkraken_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -91,6 +96,7 @@ echo "-----------------------------------------------"
 # GoogleChrome
 echo "GOOGLE CROME INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/googleChrome_installer.sh
+sudo chmod +x googleChrome_installer.sh
 ./googleChrome_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -100,6 +106,7 @@ echo "-----------------------------------------------"
 # Java jdk 11
 echo "JAVA JDK 11 INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/javaJdk11_installer.sh
+sudo chmod +x javaJdk11_installer.sh
 ./javaJdk11_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -109,6 +116,7 @@ echo "-----------------------------------------------"
 # Light
 echo "LIGHT INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/light_installer.sh
+sudo chmod +x light_installer.sh
 ./light_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -118,6 +126,7 @@ echo "-----------------------------------------------"
 # Node + Gtop
 echo "NODEJS INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/nodeJs.sh
+sudo chmod +x nodeJs.sh
 ./nodeJs.sh
 sudo npm install gtop -g
 # ---------------------------------------------------
@@ -128,6 +137,7 @@ echo "-----------------------------------------------"
 # Nvidia Driver
 echo "NVIDIA DRIVER INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/nvidiaDriver_installer.sh
+sudo chmod +x nvidiaDriver_installer.sh
 ./nvidiaDriver_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -137,6 +147,7 @@ echo "-----------------------------------------------"
 # Oh-My-Zsh
 # echo "OH-MY-ZSH INSTALLER"
 # wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/oh-my-zsh_installer.sh
+# sudo chmod +x oh-my-zsh_installer.sh
 # ./oh-my-zsh_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -146,6 +157,7 @@ echo "-----------------------------------------------"
 # Playerctl
 echo "PLAYERCTL INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/playerctl_installer.sh
+sudo chmod +x playerctl_installer.sh
 ./playerctl_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -155,6 +167,7 @@ echo "-----------------------------------------------"
 # Rambox
 echo "RAMBOX INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/rambox_installer.sh
+sudo chmod +x rambox_installer.sh
 ./rambox_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -164,6 +177,7 @@ echo "-----------------------------------------------"
 # Skype
 echo "SKYPE INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/skype_installer.sh
+sudo chmod +x skype_installer.sh
 ./skype_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -173,6 +187,7 @@ echo "-----------------------------------------------"
 # Steam
 echo "STEAM INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/steam_installer.sh
+sudo chmod +x steam_installer.sh
 ./steam_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -182,6 +197,7 @@ echo "-----------------------------------------------"
 # Virtualbox
 echo "VIRTUALBOX INSTALLER"
 wget https://raw.githubusercontent.com/Wabri/oh-my-installer/blob/master/oh-my-installer-script/virtualbox_installer.sh
+sudo chmod +x virtualbox_installer.sh
 ./virtualbox_installer.sh
 # ---------------------------------------------------
 echo "-----------------------------------------------"
