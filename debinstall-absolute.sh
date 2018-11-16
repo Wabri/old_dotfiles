705120ec3237bf0be79c55772c9c0e0f285924de
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-11-16 18:32:20 +0100

    let's try this

--- debinstall-absolute.sh
+++ debinstall-absolute.sh
@@ -8,7 +8,108 @@ sudo apt install xclip transmission thunar redshift-gtk rxvt-unicode-256color
 sudo apt install faba-icon-theme gnome-themes-standard arc-theme numix-gtk-theme numix-icon-theme fonts-font-awesome
 sudo apt install tty-clock compton ranger lxappearance arandr wget screenfetch neofetch curl git docker firefox-esr evince
 sudo apt install libxss1 libappindicator1 libindicator7 wmctrl build-essential checkinstall
-sudo apt install libc++1 ffmpeg obs-studio
+# sudo apt install libc++1 ffmpeg obs-studio
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+# Yapi install
+sudo apt install python3
+git clone https://github.com/Wabri/YAPI.git
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# Atom installer
+echo "ATOM INSTALL & CONFIGURE"
+python yapi.py install atom
+sudo apt install python-pip
+sudo pip install pep8 autopep8 flake8 flake8-docstrings hacking
+apm install linter linter-flake8 sort-lines highlight-line hyperclick seti-ui seti-syntax minimap autocomplete-python script linter-cpplint atom-beautify hey-pane
+sudo curl -sSL https://get.docker.com/ | sh
+sudo docker pull unibeautify/autopep8
+sudo docker pull unibeautify/beautysh
+sudo docker pull unibeautify/uncrustify
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "DISCORD INSTALL"
+python yapi.py install discord
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "DROPBOX INSTALL"
+python yapi.py install dropbox
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "JAVA JDK 11 INSTALL AND PATH SETUP"
+python yapi.py install javaJdk11
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "LIGHT INSTALL"
+python yapi.py install light
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "LIGHT INSTALL"
+python yapi.py install nodeJs
+sudo npm install gtop
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "NVIDIADRIVER INSTALL"
+python yapi.py install nvidiaDriver
+sudo npm install gtop
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "PLAYERCTL INSTALL"
+python yapi.py install playerctl
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "RAMBOX INSTALL"
+python yapi.py install rambox
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "SKYPE INSTALL"
+python yapi.py install skype
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "STEAM INSTALL"
+python yapi.py install steam
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "VIRTUALBOX INSTALL"
+python yapi.py install virtualbox
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
@@ -29,6 +130,8 @@ sudo cp dotfiles/scripts/spotifyWithFirefox/spotify /usr/local/bin/spotify
 
 sudo cp -r dotfiles/lightdm/ /etc/
 
+cp -r dotfiles/.atom/ ~/
+
 cp dotfiles/.gtkrc-2.0 $HOME/
 cd ~
 # ---------------------------------------------------
@@ -36,33 +139,6 @@ echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
-# Atom installer
-echo "ATOM INSTALLER"
-curl -sL https://raw.githubusercontent.com/Wabri/YAPI/master/scripts/atom.sh | sudo bash -
-sudo apt install python-pip
-sudo pip install pep8 autopep8 flake8 flake8-docstrings hacking
-apm install linter linter-flake8 sort-lines highlight-line hyperclick seti-ui seti-syntax minimap autocomplete-python script linter-cpplint atom-beautify hey-pane
-sudo curl -sSL https://get.docker.com/ | sh
-sudo docker pull unibeautify/autopep8
-sudo docker pull unibeautify/beautysh
-sudo docker pull unibeautify/uncrustify
-cp -r dotfiles/.atom/ ~/
-# ---------------------------------------------------
-echo "-----------------------------------------------"
-echo " "
-echo "-----------------------------------------------"
-# ---------------------------------------------------
-# Yapi install
-sudo apt install python3
-git clone https://github.com/Wabri/YAPI.git
-cd YAPI
-python3 yapi.py
-# ---------------------------------------------------
-echo "-----------------------------------------------"
-echo " "
-echo "-----------------------------------------------"
 sudo apt upgrade --fix-broken
-
-rm -r *.sh
 
 sudo rm -r dotfiles/
