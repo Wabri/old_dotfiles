fe3c1a57edb4f4b98902123f844e6f7df24f115f
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-11-28 16:11:52 +0100

    update with mailspring and minor changes

--- debinstall-absolute.sh
+++ debinstall-absolute.sh
@@ -8,7 +8,7 @@ sudo apt install xclip transmission thunar redshift-gtk rxvt-unicode-256color
 sudo apt install faba-icon-theme gnome-themes-standard arc-theme numix-gtk-theme numix-icon-theme fonts-font-awesome
 sudo apt install tty-clock compton ranger lxappearance arandr wget screenfetch neofetch curl git docker firefox-esr evince
 sudo apt install libxss1 libappindicator1 libindicator7 wmctrl build-essential checkinstall
-# sudo apt install libc++1 ffmpeg obs-studio
+sudo apt install libc++1 ffmpeg obs-studio
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
@@ -17,14 +17,14 @@ echo "-----------------------------------------------"
 # Yapi install
 sudo apt install python3
 git clone https://github.com/Wabri/YAPI.git
-cd YAPI
+cd YAPI/
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # Atom installer
 echo "ATOM INSTALL & CONFIGURE"
-python yapi.py install atom
+python3 yapi.py install atom
 sudo apt install python-pip
 sudo pip install pep8 autopep8 flake8 flake8-docstrings hacking
 apm install linter linter-flake8 sort-lines highlight-line hyperclick seti-ui seti-syntax minimap autocomplete-python script linter-cpplint atom-beautify hey-pane
@@ -38,35 +38,35 @@ echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "DISCORD INSTALL"
-python yapi.py install discord
+python3 yapi.py install discord
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "DROPBOX INSTALL"
-python yapi.py install dropbox
+python3 yapi.py install dropbox
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "JAVA JDK 11 INSTALL AND PATH SETUP"
-python yapi.py install javaJdk11
+python3 yapi.py install javaJdk11
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "LIGHT INSTALL"
-python yapi.py install light
+python3 yapi.py install light
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
-echo "LIGHT INSTALL"
-python yapi.py install nodeJs
+echo "NODEJS INSTALL"
+python3 yapi.py install nodeJs
 sudo npm install gtop
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -74,7 +74,7 @@ echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "NVIDIADRIVER INSTALL"
-python yapi.py install nvidiaDriver
+python3 yapi.py install nvidiaDriver
 sudo npm install gtop
 # ---------------------------------------------------
 echo "-----------------------------------------------"
@@ -82,35 +82,52 @@ echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "PLAYERCTL INSTALL"
-python yapi.py install playerctl
+python3 yapi.py install playerctl
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "RAMBOX INSTALL"
-python yapi.py install rambox
+python3 yapi.py install rambox
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "SKYPE INSTALL"
-python yapi.py install skype
+python3 yapi.py install skype
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "STEAM INSTALL"
-python yapi.py install steam
+python3 yapi.py install steam
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
 echo "-----------------------------------------------"
 # ---------------------------------------------------
 echo "VIRTUALBOX INSTALL"
-python yapi.py install virtualbox
+python3 yapi.py install virtualbox
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "MAILSPRING INSTALL"
+python3 yapi.py install mailspring
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+echo "-----------------------------------------------"
+echo " "
+echo "-----------------------------------------------"
+# ---------------------------------------------------
+sudo apt --fix-broken install
 # ---------------------------------------------------
 echo "-----------------------------------------------"
 echo " "
@@ -124,8 +141,8 @@ mkdir -p .config/i3
 
 cp -r dotfiles/i3/ ~/.config
 
-cp -r dotfiles/.urxvt ~/
-cp dotfiles/.Xresources ~/.Xresources
+#cp -r dotfiles/.urxvt ~/
+#cp dotfiles/.Xresources ~/.Xresources
 
 sudo cp dotfiles/scripts/spotifyWithFirefox/spotify /usr/local/bin/spotify
 
