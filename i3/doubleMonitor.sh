bd0f97e1961b3d8f253410002840d6916d6db308
Gabriele Puliti <gabriele.puliti@gmail.com>
2018-12-05 11:25:36 +0100

    some changes

---
+++ i3/doubleMonitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --off --output HDMI-1 --mode 1280x720 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output HDMI-2 --off
