d613750a7a80f10b8df23f802dccae5df3fa5947
Gabriele Puliti <gabriele.puliti@gmail.com>
2019-03-18 16:26:04 +0100

    fix (lightdm): fix background path for lightdm

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -3,18 +3,21 @@ cp ~/.config/Code/User/keybindings.json ./.config/Code/User/
 cp ~/.config/Code/User/settings.json ./.config/Code/User/
 code --list-extensions | xargs -L 1 echo code --install-extension > ./.config/Code/install_extension.sh
 
-# Update i3 config
+# Update i3
 # cp ~/.config/i3/ ./.config/i3/
 
-# Update gtk config
+# Update gtk
 # cp ~/.config/gtk-3.0/settings.ini
 
-# Update i3Blocks config
+# Update i3blocks
 # cp ~/.config/i3blocks/ ./.config/i3blocks/
 
 # Update .Xresources
 # cp ~/.Xresources .
-# cp ~/.urxvt/ext/* ./urxvt/ext/
+# cp ~/.urxvt .
 
 # Update .vimrc
 # cp ~/.vimrc .
+
+# Update lightdm
+# cp /etc/lightdm/ .
