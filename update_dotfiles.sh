1d4854c74a637f3d33c0954d274c00d84a40de7c
Wabri <gabriele.puliti@gmail.com>
2019-08-23 22:27:38 +0200

    Update (bashrc)

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -8,27 +8,19 @@ cp -r ~/.config/i3/ home/.config/
 # Update i3
 cp -r ~/.screenlayout/ home/
 
-# Update rofi
-cp -r ~/.config/rofi/ home/.config/
-
 # Update albert
 cp -r ~/.config/albert/albert.conf home/.config/albert/
 
-# Update i3blocks
-cp -r ~/.config/i3blocks/ home/.config/
-
 # Update polybar
 cp -r ~/.config/polybar/ home/.config/
-
-# Update i3battery
-cp -r ~/.config/i3battery home/.config/
 
 # Update .Xresources + .urxvt
 cp -r ~/.Xresources home/
 cp -r ~/.urxvt home/
 
-# Update .zshrc
+# Update shells
 cp ~/.zshrc home/
+cp ~/.bashrc home/
 
 # Update .vimrc
 cp ~/.vimrc home/
@@ -37,9 +29,4 @@ cp ~/.vimrc home/
 cp -r ~/.config/vifm/vifmrc home/.config/vifm/
 cp -r ~/.config/vifm/scripts home/.config/vifm/
 cp -r ~/.config/vifm/colors home/.config/vifm/
-
-# Update lightdm
-# this is commented because I didn't use lightdm no more
-# cp -r /etc/lightdm/ .
-# rm -r lightdm/keys.conf lightdm/lightdm.conf lightdm/users.conf
 
